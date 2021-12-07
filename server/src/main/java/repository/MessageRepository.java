9523c9a69876d4f985a5f24a2163be2da61f14f8
mariokolev <mario.kolevv@gmail.com>
2021-12-07 18:18:38 +0200

    Created class which interacts with database table message

---
+++ server/src/main/java/repository/MessageRepository.java
@@ -0,0 +1,38 @@
+package repository;
+
+import config.EntityManagerConfig;
+import entity.Message;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+public class MessageRepository {
+
+    public List<Message> findAllByConversationId(Long id) {
+        EntityManager entityManager = EntityManagerConfig.getInstance().createEntityManager();
+        TypedQuery<Message> query = entityManager.createQuery("SELECT m FROM Message m WHERE m.conversation.id = :id order by id DESC", Message.class);
+        List<Message> messages = query.setParameter("id", id).getResultList();
+
+        entityManager.close();
+        return messages;
+    }
+
+    public Message save(Message message) {
+        EntityManager entityManager = EntityManagerConfig.getInstance().createEntityManager();
+        entityManager.getTransaction().begin();
+        entityManager.persist(message);
+
+        entityManager.close();
+        return message;
+    }
+
+    public Message update(Message message) {
+        EntityManager entityManager = EntityManagerConfig.getInstance().createEntityManager();
+        entityManager.getTransaction().begin();
+        entityManager.merge(message);
+
+        entityManager.close();
+        return message;
+    }
+}
