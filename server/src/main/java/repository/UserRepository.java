7ad40f995ba4ea6e3de906eeea8af3a2ca5df22d
mariokolev <mario.kolevv@gmail.com>
2021-12-07 17:59:51 +0200

    Find by username

--- server/src/main/java/repository/UserRepository.java
+++ server/src/main/java/repository/UserRepository.java
@@ -1,4 +1,20 @@
 package repository;
 
+import config.EntityManagerConfig;
+import entity.User;
+
+import javax.persistence.EntityManager;
+import javax.persistence.TypedQuery;
+
 public class UserRepository {
+
+    public User findByUsername(String username) {
+        EntityManager entityManager = EntityManagerConfig.getInstance().createEntityManager();
+
+        TypedQuery<User> query = entityManager.createQuery("SELECT u FROM User u WHERE u.username = :username", User.class);
+        User user = query.setParameter("username", username).getSingleResult();
+
+        entityManager.close();
+        return user;
+    }
 }
