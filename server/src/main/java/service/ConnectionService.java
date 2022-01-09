603014708280da06fefe4b0db488828298db77fb
mariokolev <mario.kolevv@gmail.com>
2022-01-09 16:03:28 +0200

    added needed methods

--- server/src/main/java/service/ConnectionService.java
+++ server/src/main/java/service/ConnectionService.java
@@ -1,2 +1,40 @@
-package service;public class ConnectionService {
+package service;
+
+import dto.UserDTO;
+import mapper.UserMapper;
+import repository.ConnectionRepository;
+
+import java.util.HashMap;
+import java.util.List;
+
+public class ConnectionService {
+    private final ConnectionRepository connectionRepository;
+
+    public ConnectionService(ConnectionRepository connectionRepository) {
+        this.connectionRepository = connectionRepository;
+    }
+
+    public List<UserDTO> findAllByUserIdAndStatus(Long id, String status) {
+        return UserMapper.convertEntityToDto(connectionRepository.findAllByUserId(id, status));
+    }
+
+    public UserDTO update(HashMap<String, Object> parameters, Long senderId) {
+        Long requesterId = Long.parseLong(String.valueOf(parameters.get("requesterId")));
+        Long recipientId = Long.parseLong(String.valueOf(parameters.get("recipientId")));
+        String status = (String) parameters.get("status");
+        String oldStatus = (String) parameters.get("oldStatus");
+
+        return UserMapper.convertEntityToDto(connectionRepository.update(requesterId, recipientId, senderId, status, oldStatus));
+    }
+
+    /**
+     *
+     * @param userIds Holds requesterId and receiverId
+     * @return User with receiverId
+     */
+    public UserDTO save(HashMap<String, Object> userIds) {
+        Long requesterId = Long.parseLong(String.valueOf(userIds.get("requesterId")));
+        Long recipientId = Long.parseLong(String.valueOf(userIds.get("recipientId")));
+        return UserMapper.convertEntityToDto(connectionRepository.save(requesterId, recipientId));
+    }
 }
