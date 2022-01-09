bd2fd4484a77f02a309ba4d24ee1bf1b2c7f6eb7
mariokolev <mario.kolevv@gmail.com>
2022-01-09 20:12:23 +0200

    additional message

--- server/src/main/java/common/Messages.java
+++ server/src/main/java/common/Messages.java
@@ -5,4 +5,5 @@ public class Messages {
     public static final String USER_NOT_FOUND_USERNAME = "User with username: %s not found.";
     public static final String CONVERSATION_BY_USER_ID_NOT_FOUND = "Conversations of user with id: %d are not found.";
     public static final String CONVERSATION_NOT_FOUND = "Conversation with id: %d is not found.";
+    public static final String NO_RECEIVERS_FOUND = "No receivers found";
 }
